63cf46090daa6c487d12b2c6366651955b7d21be
wwwooo <malinovsvska@gmail.com>
2016-08-27 17:26:26 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -5,34 +5,32 @@ var mseconds = document.querySelector('.mseconds');
 var btnStart = document.querySelector('.btn-start');
 var btnStop = document.querySelector('.btn-stop');
 
-var timerSecontId;
+var timerId;
+
+var stopWatch = function(sec, min, hour) {
+    if (sec === 60) {
+        sec = 0;
+        if (min === 60) {
+            min = 0;
+            hours.innerHTML = (hour < 10) ? '0' + hour : hour;
+            hour++;
+        }
+        minutes.innerHTML = (min < 10) ? '0' + min : min;
+        min++;
+    }
+    seconds.innerHTML = (sec < 10) ? '0' + sec : sec;
+    sec++;
+
+    timerId = setTimeout(stopWatch, 1000, sec, min, hour);
+};
 
 var startCount = function() {
-    var sec = 55;
-    var min = 55;
-    var hour = 1;
-    timerSecondId = setInterval(function() {
-        if (sec === 60) {
-            sec = 0;
-            setTimeout(null, 5000);
-            if (min === 60) {
-                min = 0;
-                setTimeout(null, 5000);
-                hours.innerHTML = (hour < 10) ? '0' + hour : hour;
-                hour++;
-            }
-            minutes.innerHTML = (min < 10) ? '0' + min : min;
-            min++;
-        }
-        seconds.innerHTML = (sec < 10) ? '0' + sec : sec;
-        sec++;
-    }, 50);
+    timerId = setTimeout(stopWatch, 1000, 0, 1, 1);
 };
 
 var stopCount = function() {
-    clearInterval(timerSecondId);
+    clearTimeout(timerId);
 };
-
 
 btnStart.addEventListener('click', startCount);
 btnStop.addEventListener('click', stopCount);
