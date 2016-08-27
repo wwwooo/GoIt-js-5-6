9f3d3ab7ad8a725ba5278f7a93f015a0d7561860
wwwooo <malinovsvska@gmail.com>
2016-08-27 17:54:24 +0300

    update main.js --- var default = 0

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ var btnStop = document.querySelector('.btn-stop');
 var timerId;
 
 var startCount = function() {
-    var hour = 1;
-    var min = 1;
+    var hour = 0;
+    var min = 0;
     var sec = 0;
 
     timerId = setInterval(function() {
@@ -17,14 +17,14 @@ var startCount = function() {
             sec = 0;
             if (min === 60) {
                 min = 0;
-                hours.innerHTML = (hour < 10) ? '0' + hour : hour;
                 hour++;
+                hours.innerHTML = (hour < 10) ? '0' + hour : hour;
             }
-            minutes.innerHTML = (min < 10) ? '0' + min : min;
             min++;
+            minutes.innerHTML = (min < 10) ? '0' + min : min;
         }
-        seconds.innerHTML = (sec < 10) ? '0' + sec : sec;
         sec++;
+        seconds.innerHTML = (sec < 10) ? '0' + sec : sec;
     }, 1000);
 };
 
